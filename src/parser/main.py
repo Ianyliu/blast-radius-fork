075b999731385791b98bde673d3d9f70bdc98e5b
Simon Olander <simon.olander@netlight.com>
2023-02-06 12:48:52 +0100

    Adds json reading formatting to pd

--- src/parser/main.py
+++ src/parser/main.py
@@ -1,31 +1,26 @@
-from pyvis.network import Network
-import networkx as nx
 import re
 import json
 import pandas as pd
-#g = Network(height=800, width=800)
-data = None
-with open("plan.json", "r") as f:
-  data = json.load(f)
 
-resources = pd.json_normalize(data["values"]["root_module"]["resources"])
-#print(resources[["address", "depends_on"]])
-d = []
-for _, row in resources[["address", "depends_on"]].iterrows():
-  targets = row["depends_on"]
-  if type(targets) == list :
-    for target in targets:
-      d.append([row["address"], target])
+def plan():
+  data = None
+  with open("plan.json", "r") as f:
+    data = json.load(f)
 
-edges = pd.DataFrame(d, columns=["source", "target"])
+  HEXCODES = ["#007bff","#6610f2","#6f42c1","#e83e8c","#dc3545","#fd7e14","#ffc107","#28a745","#20c997","#17a2b8","#fff","#6c757d","#343a40","#007bff","#6c757d","#28a745","#17a2b8","#ffc107","#dc3545","#f8f9fa","#343a40"]
 
-print(edges)
-"""
-data = re.sub(r"\\\"", "", data)
-g.use_DOT = True
-data = " ".join(data.splitlines())
-data = data.replace('"', '\\"')
-g.dot_lang = data
+  resources = pd.json_normalize(data["values"]["root_module"]["resources"])
+  colors = pd.DataFrame(zip(resources["type"].unique(), HEXCODES), columns=["type", "colour"])
+  nodes = resources.merge(colors, how='left', on="type")
+  d = []
+  for _, row in resources[["address", "depends_on"]].iterrows():
+    targets = row["depends_on"]
+    if type(targets) == list :
+      for target in targets:
+        d.append([row["address"], target])
 
-g.show("index.html")
-"""
+  edges = pd.DataFrame(d, columns=["source", "target"])
+  return edges, nodes
+
+if __name__=="__main__":
+  print(plan())
